f73370b20f747483e95ca1ac204c753727bd0364
christianalim <christiananthonylim1@gmail.com>
2019-10-10 14:48:20 -0500

    some framing

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+    
+    return (
+        <div>
+            <h1>About</h1>
+            <h2>Hi. I'm Christian.</h2>
+            <img src={require("../assets/images/profile.jpg")} height="100" width="100" />
+            <p> I'm a full-stack software engineer with experience using React, Redux, and Ruby on Rails. I found web development through working with others to create unique solutions for problems in my finance career. With a background in trading, I am accustomed to working under pressure and having to think quickly and critically to succeed. I strive to bring my abundant curiosity and passion for solving interesting problems to web development.</p>
+
+        </div>
+    )
+}
+
+export default About;
