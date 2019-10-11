f71263aa053b9b64e2f8a68274d551a08b003b34
christianalim <christiananthonylim1@gmail.com>
2019-10-11 12:30:56 -0500

    added more structure

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,26 @@ const About = () => {
     
     return (
         <div style={styles.container}>
-            <h1>About</h1>
-            <h2>Hi. I'm Christian.</h2>
-            <img src={require("../assets/images/profile.jpg")} height="100" width="100" />
-            <p> I'm a full-stack software engineer with experience using React, Redux, and Ruby on Rails. I found web development through working with others to create unique solutions for problems in my finance career. With a background in trading, I am accustomed to working under pressure and having to think quickly and critically to succeed. I strive to bring my abundant curiosity and passion for solving interesting problems to web development.</p>
+            <div style={styles.profile}>
+                <h1>Hi. I'm Christian.</h1>
+                <img src={require("../assets/images/profile.jpg")} style={styles.profileImage} />
+
+                <div style={styles.info}>
+                    <p> I'm a full-stack software engineer with experience using React, Redux, and Ruby on Rails. I found web development through working with others to create unique solutions for problems in my finance career. With a background in trading, I am accustomed to working under pressure and having to think quickly and critically to succeed. I strive to bring my abundant curiosity and passion for solving interesting problems to web development.</p>
+                </div>
+                <div style={styles.technologies}>
+                    <h4>Here are a few technologies I've been working with:</h4>
+                    <ul style={styles.ul}>
+                        <li>JavaScript (ES6+)</li>
+                        <li>React</li>
+                        <li>React Native</li>
+                        <li>HTML5 & CSS</li>
+                        <li>Ruby</li>
+                        <li>Ruby on Rails</li>
+                        <li>PostgreSQL</li>
+                    </ul>
+                </div>
+            </div>
 
         </div>
     )
@@ -17,10 +33,29 @@ const About = () => {
 const styles = {
     container:{
         display:'grid',
-        gridTemplateColumns: '30% 40% 30%',
-        gridTemplateRows: '33% 33% 33%',
+        gridTemplateColumns: '15% 70% 15%',
+        gridTemplateRows: '50% 50%',
         width: '100%',
         backgroundColor: '#F7F7F2',
+    },
+    profile:{
+        gridColumnStart: 2,
+        gridRowStart: 1,
+        textAlign:'center',
+    },
+    profileImage:{
+        height: '20rem',
+        width: '20rem',
+        borderRadius: 10,
+    },
+    info:{
+        textAlign:'start',
+        borderStyle: 'solid',
+        borderWidth: '0px 0px 2px 0px',
+        borderColor: 'black',
+    },
+    ul:{
+        listStyleType: 'none',
     }
 }
 
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,19 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
+import YouTube from 'react-youtube';
+import StudyHub from './StudyHub';
 
-export default class Work extends Component {
-    render() {
+
+const Work = () => {
+
         return (
-            <div>
-                <h1>Work</h1>
-                <p>Check out some of my recent projects:</p>
-                <h2>StudyHub</h2>
-                <p>Description:</p>
-                <h2>Tickr</h2>
-                <p>Description:</p>
-                <h2>RetroJS</h2>
-                <p>Description:</p>
+            <div style={styles.container}>
+                <div style={styles.title}>
+                    <h1>Here's some things I've built:</h1>
+                </div>
+                <StudyHub />
+                
 
             </div>
         )
-    }
 }
+
+const styles = {
+    container:{
+        display:'grid',
+        gridTemplateColumns: '10% 80% 10%',
+        width: '100%',
+        backgroundColor: '#9BC4CB',
+    },
+    title:{
+        gridColumnStart: 2,
+    },
+    
+}
+
+
+export default Work;
+
