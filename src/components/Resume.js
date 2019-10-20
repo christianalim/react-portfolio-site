d6459610b1f4c716d267a2cc811f775f17305d23
christianalim <christiananthonylim1@gmail.com>
2019-10-20 11:36:03 -0400

    fix resume link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,11 +3,20 @@ import React from 'react'
 const Resume = () => {
 
     return (
-        <div>
+        <div style={styles.container}>
             <h1>Resume</h1>
-            <a href="../assets/resume.pdf" download>Download Resume!</a>
+            <a href="/resume.pdf" download>Download Resume!</a>
+            
         </div>
     )
 }
+
+const styles = {
+    container:{
+        display:'flex',
+        alignItems:'center'
+    }
+}
+
 
 export default Resume;
